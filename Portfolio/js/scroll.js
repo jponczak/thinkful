02ed19f741a1fa9c95fd9010c120372f45be0eb5
Joe Ponczak <jponczak@gmail.com>
2020-02-16 12:18:00 -0500

    added jquery scroll to top, minor css updates, fixed google font html error

---
+++ Portfolio/js/scroll.js
@@ -0,0 +1,27 @@
+/* controls flow */
+function init() {
+    scrollFunction();
+    moveToTop();
+}
+
+function scrollFunction() {
+    /* checks to see if the user scrolls */
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 100) {
+            $('#topBtn').css('display','block');
+        } else {
+            $('#topBtn').css('display','none');
+        }
+    });
+  }
+
+  function moveToTop() {
+      /* scrolls to the top of the page */
+    $('body').on('click','#topBtn', function() {
+        $(window).scrollTop(1);
+        $('#topBtn').css('display','none');
+    });
+}
+
+$(init)
