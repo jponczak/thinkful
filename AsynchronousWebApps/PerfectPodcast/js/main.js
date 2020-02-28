aed38f752b7a3f7a0022f3f776a0501d4eca6c15
Joe Ponczak <jponczak@gmail.com>
2020-02-27 20:48:57 -0500

    updating javascript

--- AsynchronousWebApps/PerfectPodcast/js/main.js
+++ AsynchronousWebApps/PerfectPodcast/js/main.js
@@ -3,20 +3,17 @@
 var listenNowPodcasts = [];
 
 let renderMapQuestResults = (responseJson, userData) => {
-  // console.log(userData);
-  // console.log(responseJson.route.distance);
-  // console.log(responseJson.route.formattedTime);
-
   let legs = responseJson.route.legs[0].maneuvers;
-  $('.results').removeClass('hidden');
-  $('.podcast-result').removeClass('hidden');
+  // $('.results').removeClass('hidden');
+  // $('.podcast-result').removeClass('hidden');
 
   let htmlResults = '';
   for (let x = 0; x < legs.length; x++) {
     htmlResults += `${x + 1}. ${legs[x].narrative}<br />`
   }
 
-  $('.mapquest-results').html(htmlResults);
+  // $('.mapquest-results').html(htmlResults);
+  $('.directions-text-inner').html(htmlResults);
 }
 
 let renderListenNowResults = (podcastData) => {
@@ -26,7 +23,7 @@ let renderListenNowResults = (podcastData) => {
     htmlResults += `${podcastData[x].title_original}<br />`;
   }
 
-  $('.podcast-results').html(htmlResults);
+  $('.podcasts-text-inner').html(htmlResults);
 
 }
 
@@ -48,19 +45,46 @@ let watchForm = () => {
       userData.mapquestUrl = mapquestBaseURL + '?' + buildAddressQueryString(userData);
       getAddressApi(userData);
     } else console.log('false');
+
+    var direction = $('#directions-text').position().top;
+
+    $('html, body').animate({
+      scrollTop: direction
+    }, 4500);
+  
   });
+
+
 }
 
 $(function () {
   console.log('App loaded! Waiting for submit!');
   //smooth scroll
-  $('#view-work').on('click', function() {
-    var images = $('#images').position().top;
+  $('#directions').on('click', function() {
+    var direction = $('#directions-text').position().top;
+    var podcast = $('#podcasts-text').position().top;
+    var find = $('#find').position().top;
 
     $('html, body').animate({
-      scrollTop: images
+      scrollTop: direction
+    }, 4500);
+    $('html, body').animate({
+      scrollTop: podcast
+    }, 4500);
+    $('html, body').animate({
+      scrollTop: find
+    }, 2300);
+  });
+
+  $('#find-pods').on('click', function() {
+    console.log('cliek');
+    var gotoForm = $('.flex-outer').position().top;
+
+    $('html, body').animate({
+      scrollTop: gotoForm
     }, 900);
   });
+
   watchForm();
 
 });
